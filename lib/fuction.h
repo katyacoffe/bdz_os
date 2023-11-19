b6ec64dd4504a001c5e78d4cfee009eac9c77aca
emelnikova <ekaterina@Emelnikova>
2023-11-20 02:43:52 +0300

    add findShortestPathLength function in lib

--- lib/fuction.h
+++ lib/fuction.h
@@ -1,2 +1,5 @@
 bool isValid(vector<vector<int>> const &mat, vector<vector<bool>> &visited,
 		int row, int col);
+
+int findShortestPathLength(vector<vector<int>> const &mat, pair<int, int> &src,
+					pair<int, int> &dest);
