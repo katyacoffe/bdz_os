37fe3225f73e1204142a51fbd72c2cb04c96df5b
emelnikova <ekaterina@Emelnikova>
2023-11-20 03:11:27 +0300

    rename header

--- src/lib/func.h
+++
@@ -1,5 +0,0 @@
-bool isValid(vector<vector<int>> const &mat, vector<vector<bool>> &visited,
-		int row, int col);
-
-int findShortestPathLength(vector<vector<int>> const &mat, pair<int, int> &src,
-					pair<int, int> &dest);
