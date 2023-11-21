33cdb04c267d5d9d1714a3dc6db4acb97c5e9c98
emelnikova <ekaterina@Emelnikova>
2023-11-21 03:27:14 +0300

    program worked

--- src/lib/function.h
+++ src/lib/function.h
@@ -3,6 +3,13 @@
 #include <vector>
 #include <climits>
 #include <cstring>
+#include <fstream>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <unistd.h>
+
 using namespace std;
 
 // A Queue Node
@@ -18,3 +25,5 @@ bool isValid(std::vector<vector<int>> const &mat, vector<vector<bool>> &visited,
 
 int findShortestPathLength(std::vector<vector<int>> const &mat, pair<int, int> &src,
 					pair<int, int> &dest);
+
+void monitoring();
--- src/make.cpp
+++ src/make.cpp
@@ -2,9 +2,12 @@
 
 // Below arrays detail all four possible movements from a cell
 
-
+mutex mtx;
 int main()
 {
+	thread th(monitoring);
+	
+
 	vector<vector<int>> mat =
 	{
 		{ 1, 1, 1, 1, 1, 0, 0, 1, 1, 1 },
@@ -21,10 +24,13 @@ int main()
 
 	pair<int, int> src;
 	pair<int, int> dest;
+	mtx.lock();
+
 	cout << "Введите начальные координаты" << endl;
 	cin >> src.first >> src.second;
 	cout << "Введите конечные координаты" << endl;
 	cin >> dest.first >> dest.second;
+	mtx.unlock();
 	int min_dist = findShortestPathLength(mat, src, dest);
 	if (min_dist != -1)
 	{
@@ -35,6 +41,7 @@ int main()
 		cout << "Путь не может быть найден при заданных параметрах" << endl;
 		return main();
 	}
+	th.join();
 
 	return 0;
 }
