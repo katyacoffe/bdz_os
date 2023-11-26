0744d43f0cb9b971a7846693b1f5b0ff6f4fc138
katyacoffe <150744385+katyacoffe@users.noreply.github.com>
2023-11-27 02:37:57 +0300

    Update for draw path f2.cpp

--- src/f2.cpp
+++ src/f2.cpp
@@ -12,9 +12,13 @@ int findShortestPathLength(vector<vector<int>> const &mat, pair<int, int> &src,
 	}
 
 	// `M × N` matrix
+	
 	int M = mat.size();
 	int N = mat[0].size();
-
+	
+	vector<vector<int>> mat2 = mat;
+	
+	
 	// construct a `M × N` matrix to keep track of visited cells
 	vector<vector<bool>> visited;
 	visited.resize(M, vector<bool>(N));
@@ -39,10 +43,14 @@ int findShortestPathLength(vector<vector<int>> const &mat, pair<int, int> &src,
 		// dequeue front node and process it
 		Node node = q.front();
 		q.pop();
-
+		
 		// (i, j) represents a current cell, and `dist` stores its
 		// minimum distance from the source
 		int i = node.x, j = node.y, dist = node.dist;
+		
+		if (mat[i][j]!=0 ){
+			mat2[i][j] = dist;
+		}
 
 		// if the destination is found, update `min_dist` and stop
 		if (i == dest.first && j == dest.second)
@@ -65,8 +73,60 @@ int findShortestPathLength(vector<vector<int>> const &mat, pair<int, int> &src,
 			}
 		}
 	}
+	cout<<"Begin matrix"<<endl;
+	for (int p = 0; p<M; p++ )
+	{
+		for (int t = 0; t<N; t++ )
+			cout<<mat[p][t]<< "\t";
+		cout<<""<<endl;
+	}
+	cout<<"Matrix with pathes"<<endl;
+	for (int p = 0; p<M; p++ )
+	{
+		for (int t = 0; t<N; t++ )
+			cout<<mat2[p][t]<< "\t";
+		cout<<""<<endl;
+	}
 
 	if (min_dist != INT_MAX) {
+		int ii = dest.first;
+		int jj = dest.second;
+		int min = min_dist;
+		int dlina_i = abs(src.first - dest.first);
+		int dlina_j = abs(src.second - dest.second);
+		mat2[ii][jj] = 102;
+		while (dlina_i >0 || dlina_j>0){
+			if (mat2[ii][jj-1] == min - 1 && jj>=1){
+				dlina_j -=1;
+				mat2[ii][jj-1] = 101;
+				jj-=1;
+			}
+			else if (mat2[ii][jj+1] == min - 1 && jj<=N-1){
+				dlina_j -=1;
+				mat2[ii][jj+1] = 101;
+				jj+=1;
+			}
+			else if (mat2[ii-1][jj] == min - 1 && ii>=1){
+				dlina_i -=1;
+				mat2[ii-1][jj] = 101;
+				ii-=1;
+			}
+			else if (mat2[ii+1][jj] == min - 1 && ii<=M-1){
+				dlina_i -=1;
+				mat2[ii+1][jj] = 101;
+				ii+=1;
+			}
+			min-=1;
+		}
+		mat2[ii][jj] = 100;
+		cout<<"Matrix, where our path = 101\nBegin = 100\nEnd = 102"<<endl;
+		for (int p = 0; p<M; p++ )
+		{
+			for (int t = 0; t<N; t++ )
+				cout<<mat2[p][t]<< "\t";
+			cout<<""<<endl;
+		}
+
 		return min_dist;
 	}
 
